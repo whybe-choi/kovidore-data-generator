ddec7a7fda92674f09deef7ec956ef66d75eb3aa
Yongbin Choi <whybe.choi@gmail.com>
2025-12-28 11:53:21 +0000

    feat: export summary and query config builders

--- src/kovidore_data_generator/pipelines/__init__.py
+++ src/kovidore_data_generator/pipelines/__init__.py
@@ -1,5 +1,15 @@
-from kovidore_data_generator.pipelines.summary import build_summary_config
+from kovidore_data_generator.pipelines.summary import (
+    build_single_section_summary_config,
+    build_cross_section_summary_config,
+)
+from kovidore_data_generator.pipelines.query import (
+    build_query_from_summary_config,
+    build_query_from_context_config,
+)
 
 __all__ = [
-    "build_summary_config",
+    "build_single_section_summary_config",
+    "build_cross_section_summary_config",
+    "build_query_from_summary_config",
+    "build_query_from_context_config",
 ]
